4802f515ea9fdf4c89ff99555c04f7cb46131504
Gabriel Peres Lutz <gabriel.p.lutz@gmail.com>
2021-07-01 23:28:44 -0300

    Added search bar

--- src/components/Store/Store.js
+++ src/components/Store/Store.js
@@ -2,15 +2,19 @@ import styled from "styled-components"
 import axios from "axios"
 import { useEffect, useState } from "react"
 import MangaCard from "./MangaCard"
+import {DebounceInput} from 'react-debounce-input';
+import { FaSearch } from "react-icons/fa";
 
 export default function Store(){
     const [data, setData] = useState([])
     const [categories,setCategories]= useState([])
-
+    const [filteredString, setFilteredString] = useState("")
+    const [filteredData, setFilteredData] = useState([])
     useEffect(()=>{
         const promisse = axios.get(`http://localhost:4000/mangasall`)
         promisse.then(promisseData =>{
             setData(promisseData.data)
+            filterList("", true, promisseData.data)
         })
         const request = axios.get(`http://localhost:4000/categories`)
         request.then(resp=>{
@@ -25,7 +29,17 @@ export default function Store(){
         const promisse = axios.get(`http://localhost:4000/mangas${id}`)
         promisse.then(promisseData =>{
             setData(promisseData.data)
+            filterList("", true, promisseData.data)
         })
+    }
+
+    function filterList(e, redoFilter, promisseData){
+        if(redoFilter){
+            setFilteredData(promisseData.filter(m=> m.name.toLowerCase().includes(filteredString.toLowerCase())))
+        }else{
+            setFilteredString(e.target.value)
+            setFilteredData(data.filter(m=> m.name.toLowerCase().includes(e.target.value.toLowerCase())))
+        }
     }
 
     return(
@@ -34,8 +48,17 @@ export default function Store(){
                 <p onClick={()=>loadMangasByCategory("all")}>All</p>
                 {categories.map(i=><p key={i.id} onClick={()=>loadMangasByCategory(i.id)}>{i.name}</p>)}
             </Categories>
+            <Wrapper>    
+                <DebounceInput
+                    element={Search}
+                    debounceTimeout={300}
+                    onChange={e=>filterList(e, false)}
+                    placeholder="Filter by name..."
+                />
+                <FaSearch></FaSearch>
+            </Wrapper>
             <Conteiner>
-                {data.length ? data.map(m=> <MangaCard key={m.id} mangaInfo={m}></MangaCard>): "Nehum mangá disponível no momento"}
+                {filteredData.length ? filteredData.map(m=> <MangaCard key={m.id} mangaInfo={m}></MangaCard>): "Nehum mangá disponível no momento"}
             </Conteiner>
         </>
     )
@@ -45,7 +68,7 @@ const Conteiner = styled.div`
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
-    margin-top: 135px;
+    margin-top: 15px;
     width: 100%;
     padding: 0 10px;
 `
@@ -68,4 +91,26 @@ const Categories = styled.div`
     box-shadow: 0 0px 10px #474747;
     z-index: 1;
     font-size: 13px;
+`
+const Wrapper = styled.div`
+    margin-top: 150px;
+    padding: 0 25px;
+    display:flex;
+    position:relative;
+    svg{
+        position: absolute;
+        right: 35px;
+        top:10px;
+    }
+`
+const Search = styled.input`
+    width:100%;
+    height:35px;
+    border-radius: 10px;
+    padding: 0 10px;
+    font-size: 14px;
+    font-family: 'Roboto', sans-serif;
+    ::placeholder{
+        color:#a5a5a5;
+    }
 `
